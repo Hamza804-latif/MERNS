f216d4d37ee58a06390ceb5801a402b9de835683
Hamza804-latif <90188142+Hamza804-latif@users.noreply.github.com>
2023-08-07 12:54:03 +0500

    in progress

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const DatabaseConnection = require("./database/index.js");
 const { registerModel } = require("./database/models.js");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
@@ -17,17 +18,46 @@ app.post("/register", async (req, resp) => {
     if (checkEmail) {
       return resp.json({ status: 401, msg: "User is Already exist" });
     } else {
+      let hashedPassword = await bcrypt.hash(req.body.password, 10);
       await registerModel.create({
         image: req.body.image,
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
       });
       return resp.json({ status: 200, msg: "Registered Successfully!" });
     }
   } catch (error) {
     console.log("error in register", error);
     resp.json({ status: 500, msg: "Internal Server Error" });
+  }
+});
+
+app.post("/login", async (req, resp) => {
+  let { email, password } = req.body;
+  try {
+    let emailCheck = await registerModel.findOne({ email }).select("-image");
+    if (emailCheck) {
+      let checkHashedPassword = await bcrypt.compare(
+        password,
+        emailCheck.password
+      );
+      if (checkHashedPassword) {
+        return resp.json({
+          status: 200,
+          msg: "Login Successfully",
+        });
+      } else {
+        return resp.json({
+          status: 401,
+          msg: "email or password is incorrect",
+        });
+      }
+    } else {
+      return resp.json({ status: 401, msg: "email or password is incorrect" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
